f33bf98a7b82740065181ca09a45ec1ed3ae1946
“DahliaDjoumessi” <dahliadjoumessi18@gmail.com>
2023-05-07 21:52:09 +0100

    again update

--- database/factories/VoteFactory.php
+++ database/factories/VoteFactory.php
@@ -3,13 +3,11 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Carbon\Carbon;
-use Illuminate\support\str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Election>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Vote>
  */
-class ElectionFactory extends Factory
+class VoteFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -19,11 +17,7 @@ class ElectionFactory extends Factory
     public function definition(): array
     {
         return [
-            "description"=>Str::random(300),
-            "label" =>Str::random(15),
-            "statut"=>Str::random(10),
             "date" => Carbon::createFromFormat('Y-m-d', $this->faker->date('Y-m-d', '2000-01-01')),
         ];
     }
 }
-
--- database/migrations/2023_04_18_091952_create_elections_table.php
+++ database/migrations/2023_04_18_091952_create_elections_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('elections', function (Blueprint $table) {
             $table->id();
-            $table->string('description');
+            $table->string('description',1000);
             $table->string('label');
             $table->string('statut');
             $table->date('date');
